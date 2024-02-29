1c567ebf69e3909504f7752d75865fa6295d618f
Mimi2258201 <U2258201@unimail.hud.ac.uk>
2024-02-29 14:10:41 +0000

    Updated combat script and player prefab

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    [SerializeField] float reach;
+    Animator animator;
+    bool canPunch = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward * 5f);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.DrawRay(transform.position, transform.forward * reach, Color.red);
+            // Do actual raycast here and check for interface
+            Attack();
+        }
+        else
+            Debug.DrawRay(transform.position, transform.forward * reach);
     }
 
     void Attack()
     {
+        if (canPunch)
+        {
+            animator.Play("SimplePunch");
+            canPunch = false;
+        }
+    }
 
+    public void FinishAttack()
+    {
+        canPunch = true;
     }
 }
